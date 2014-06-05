3b261e3959c560e4ada2a3d0dad74dc53c3ce27b
Noritaka Baba <noribaba1121@gmail.com>
2014-06-06 00:55:06 +0900

    [r4m]空白・空白行の整理。 [r4m]文字列リテラルをシングルクォーテーションで統一。

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 
-function setIconCookie(cName){
+function setIconCookie(cName) {
   var date=new Date();
   var year = date.getFullYear();
   var nextEndOfYear = new Date(year+1, 12, 0);
-  document.cookie = cName + "=" + escape(year) + "; expires=" + nextEndOfYear.toGMTString();
+  document.cookie = cName + '=' + escape(year)
+                  + '; expires=' + nextEndOfYear.toGMTString();
 }
 
-function loadYearIcon(type){
-
+function loadYearIcon(type) {
   var postposition = '';
-  if( typeof type != "undefined" ){
-    if ( type === 'thin' ){
+  if (typeof type != 'undefined') {
+    if (type === 'thin') {
       postposition = '_helvetica-ul_yugothic-el';
-    } else if ( type == 'thin2'){
+    } else if (type == 'thin2') {
       postposition = '_helvetica-t_yugothic-l';
     }
   }
 
   var date = new Date();
   var year = date.getFullYear();
-
-  var cName = "yearcookie"+postposition;
+  var cName = 'yearcookie' + postposition;
   var findYearCookie = false;
   if (document.cookie) {
     var cookies = document.cookie.split("; ");
     for (var i = 0; i < cookies.length; i++) {
-      var str = cookies[i].split("=");
+      var str = cookies[i].split('=');
       if (str[0] == cName) {
         findYearCookie = true;
         var cookie_value = unescape(str[1]);
-        if ( cookie_value != year ){
+        if (cookie_value != year) {
           setIconCookie(cName);
           location.reload();
         }
       }
     }
   }
-  if ( !findYearCookie ){
+
+  if (!findYearCookie) {
     setIconCookie(cName);
   }
 
