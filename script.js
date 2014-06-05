7ee260cfa6b19219492c3eeec99fb2af5fc17083
PGMY <PGMYURI@gmail.com>
2014-06-05 15:08:14 +0900

    jsそのまま実行　(&インデント揃え確認)

--- script.js
+++ script.js
@@ -1,38 +1,35 @@
 
-
 function setIconCookie(cName){
   var date=new Date();
-	var year = date.getFullYear();
-	var nextEndOfYear = new Date(year+1, 12, 0);
-	document.cookie = cName + "=" + escape(year) + "; expires=" + nextEndOfYear.toGMTString();
-}
-
-window.onload = function(){
-  var date = new Date();
   var year = date.getFullYear();
-
-	var cName = "yearcookie";
-  var findYearCookie = false;
-	if (document.cookie) {
-		var cookies = document.cookie.split("; ");
-		for (var i = 0; i < cookies.length; i++) {
-			var str = cookies[i].split("=");
-			if (str[0] == cName) {
-        findYearCookie = true;
-				var cookie_value = unescape(str[1]);
-				if ( cookie_value != year ){
-					setIconCookie(cName);
-					location.reload();
-				}
-			}
-		}
-	}
-  if ( !findYearCookie ){
-		setIconCookie(cName);
-	}
-
-	var calImg = year+'.png';
-	document.getElementById('icon').setAttribute('href',calImg);
-	document.getElementById('iconimg').setAttribute('src',calImg);
-
+  var nextEndOfYear = new Date(year+1, 12, 0);
+  document.cookie = cName + "=" + escape(year) + "; expires=" + nextEndOfYear.toGMTString();
 }
+
+var date = new Date();
+var year = date.getFullYear();
+
+var cName = "yearcookie";
+var findYearCookie = false;
+if (document.cookie) {
+  var cookies = document.cookie.split("; ");
+  for (var i = 0; i < cookies.length; i++) {
+    var str = cookies[i].split("=");
+    if (str[0] == cName) {
+      findYearCookie = true;
+      var cookie_value = unescape(str[1]);
+      if ( cookie_value != year ){
+        setIconCookie(cName);
+        location.reload();
+      }
+    }
+  }
+}
+console.log("test");
+if ( !findYearCookie ){
+	setIconCookie(cName);
+}
+
+var calImg = year+'.png';
+document.getElementById('icon').setAttribute('href',calImg);
+document.getElementById('iconimg').setAttribute('src',calImg);
